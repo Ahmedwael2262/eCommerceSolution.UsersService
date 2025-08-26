b072f0ca668b37c6b3e12b444a3a5e829b2f3db0
Ahmedwael2262 <ahmeddwael372@gmail.com>
2025-08-26 22:01:48 +0200

    add enviroment variables to be good for docker yaml

--- eCommerce.Infrastructure/DbContext/DapperDbContext.cs
+++ eCommerce.Infrastructure/DbContext/DapperDbContext.cs
@@ -9,15 +9,18 @@ public class DapperDbContext
   private readonly IConfiguration _configuration;
   private readonly IDbConnection _connection;
 
-  public DapperDbContext(IConfiguration configuration)
-  {
-    _configuration = configuration;
-    string? connectionString = _configuration.GetConnectionString("PostgresConnection");
+    public DapperDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        string connectionStringTemplate = _configuration.GetConnectionString("PostgresConnection")!;
+        string connectionString = connectionStringTemplate
+          .Replace("$POSTGRES_HOST", Environment.GetEnvironmentVariable("POSTGRES_HOST"))
+          .Replace("$POSTGRES_PASSWORD", Environment.GetEnvironmentVariable("POSTGRES_PASSWORD"));
 
-    //Create a new NpgsqlConnection with the retrieved connection string
-    _connection = new NpgsqlConnection(connectionString);
-  }
+        //Create a new NpgsqlConnection with the retrieved connection string
+        _connection = new NpgsqlConnection(connectionString);
+    }
 
 
-  public IDbConnection DbConnection => _connection;
+    public IDbConnection DbConnection => _connection;
 }
